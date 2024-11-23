ae9a32c79684eaae9cc6d39870dfbf515b089d17
Rodrigogaleann <141097778+Rodrigogaleann@users.noreply.github.com>
2024-11-23 12:54:10 -0300

    Agregando los archivos

---
+++ main.js
@@ -0,0 +1,29 @@
+//el juego selecciona  un numero al azar para que adivines
+let numeroAzar= Math.floor(Math.random()*100)+1
+
+let numeroEntrada = document.getElementById("numeroEntrada")
+let mensaje = document.getElementById("mensaje")
+
+//esta accion se  va a ejecutar cuando se toque el boton chequear 
+
+function chequearResultado (){
+
+    let numeroIngresado = parseInt(numeroEntrada.value)
+
+    if (numeroIngresado === 0 || numeroIngresado > 100 || isNaN(numeroIngresado)){
+       mensaje.textContent = "Por favor, ingrese un numero valido entre 1 y 100"
+       mensaje.style.color = "red";
+       return
+    }
+    
+    if(numeroIngresado === numeroAzar){
+        mensaje.textContent= "Felicitaciones ¡ Has adivinado el numero !"
+        mensaje.style.color="Green"
+    } else if (numeroIngresado < numeroAzar){
+        mensaje.textContent= "El numero es más alto"
+        mensaje.style.color="Blue"
+    } else {
+        mensaje.textContent= "El numero es más bajo"
+        mensaje.style.color="Pink"
+    }
+}
